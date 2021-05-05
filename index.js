eef4a8a2927a21bbee42ea80eff6f01d54038df6
Joel Himanen <joel.himanen@aalto.fi>
2021-05-05 15:37:40 +0300

    Lint

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Person = require('./models/person')
 const app = express()
 
 morgan.token('post_data', (req, res) => {
-    if (req.method === 'POST') {
-        return JSON.stringify(req.body)
-    } else return null
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  } else return null
 })
 
 app.use(express.static('build'))
@@ -18,39 +18,39 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (req, res) => {
-    Person.find({}).then(people => {
-        const amt = people.length
-        const date = new Date()
-        res.send(
-            `<p>Phonebook has info for ${amt} people </p> <p> Date: ${date.toISOString().slice(0,10)} </p>`
-        )
-    })
+  Person.find({}).then(people => {
+    const amt = people.length
+    const date = new Date()
+    res.send(
+      `<p>Phonebook has info for ${amt} people </p> <p> Date: ${date.toISOString().slice(0,10)} </p>`
+    )
+  })
 })
 
 app.get('/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 app.get('/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
+  Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
@@ -58,60 +58,60 @@ app.delete('/persons/:id', (req, res, next) => {
 })
 
 app.post('/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (body.name === undefined) {
-        return res.status(400).json({ error: 'name missing' })
-    }
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'name missing' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        nr: body.nr
-    })
+  const person = new Person({
+    name: body.name,
+    nr: body.nr
+  })
 
-    person.save()
+  person.save()
     .then(savedPerson => {
-        res.json(savedPerson)
+      res.json(savedPerson)
     })
     .catch(error => next(error))
 })
 
 app.put('/persons/:id', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = {
-        name: body.name,
-        nr: body.nr
-    }
+  const person = {
+    name: body.name,
+    nr: body.nr
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).send({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,34 +1,34 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI.toString()
+const url = process.env.MONGODB_URI
 
 console.log(`Connecting to ${url}`)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('Connected to MongoDB!')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB!')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3
-    },
-    nr: {
-        type: String,
-        minlength: 8
-    }
-    })
-    
+  name: {
+    type: String,
+    minlength: 3
+  },
+  nr: {
+    type: String,
+    minlength: 8
+  }
+})
+
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    }
+  }
 })
 
 personSchema.plugin(uniqueValidator)
